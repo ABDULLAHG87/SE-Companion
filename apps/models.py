185b8b194068cb16d04f7f19a9117cea2f6c1f25
ABDULLAHG87 <hakeemabdullah87@gmail.com>
2024-02-27 22:55:28 +0100

    FEAT: add username to the required user information for database

--- apps/models.py
+++ apps/models.py
@@ -17,9 +17,11 @@ class User(UserMixin, db.Model):
     
     #This code works for the mysql database
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password_hash = db.Column(db.String(256), nullable=False)  # Increase the length of the column
-    def __init__(self, email, password):
+    def __init__(self, username, email, password):
+        self.username = username
         self.email = email
         self.password_hash = generate_password_hash(password)
 
