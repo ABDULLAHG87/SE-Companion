d91d52107061b99c30c8b97e5b4271ef3a428aca
ABDULLAHG87 <hakeemabdullah87@gmail.com>
2024-03-19 22:10:53 +0100

    REFACTOR: optimize the methods.py to comply with pep8 styling

--- apps/methods.py
+++ apps/methods.py
@@ -4,7 +4,6 @@ import requests
 import random
 
 
-
 app = Flask(__name__)
 
 # Configure Flask-Mail
@@ -16,33 +15,43 @@ app.config['MAIL_PASSWORD'] = 'abdul4prof87'
 
 mail = Mail(app)
 
+
 # Function to send password reset email
 def send_password_reset_email(email, token):
     '''Function that reset password via email'''
     # Compose email message
-    msg = Message('Password Reset Request', sender='hakeemabdullah87@gmail.com', recipients=[email])
-    msg.body = f'Click the link to reset your password: http://localhost:5000/reset_password/{token}'
+    msg = Message('Password Reset Request',
+                  sender='hakeemabdullah87@gmail.com', recipients=[email])
+    msg.body = (
+                f'Click the link to reset your password: '
+                f'http://localhost:5000/reset_password/{token}'
+                )
 
     # Send email
     mail.send(msg)
-#function to filter resources in the resources page based on filter or search query
-def filter_resources(search_query, type_filter, category_filter, level_filter,resources):
+
+
+def filter_resources(search_query, type_filter, category_filter, level_filter,
+                     resources):
     '''Function to filter resources based on search query and filters'''
     filtered_resources = resources
-    
     if search_query:
-        filtered_resources = [resource for resource in filtered_resources if search_query.lower() in resource['title'].lower()]
-        
+        filtered_resources = [resource for resource in filtered_resources
+                              if search_query.lower() in resource['title'].lower()]
+
     if type_filter and type_filter != 'Filter by Type':
-        filtered_resources = [resource for resource in filtered_resources if resource['type'] == type_filter]
-        
+        filtered_resources = [resource for resource in filtered_resources
+                              if resource['type'] == type_filter]
+
     if category_filter and category_filter != 'Filter by Category':
-        filtered_resources = [resource for resource in filtered_resources if resource['category'] == category_filter]
-        
+        filtered_resources = [resource for resource in filtered_resources
+                              if resource['category'] == category_filter]
+
     if level_filter and level_filter != 'Filter by Level':
-        filtered_resources = [resource for resource in filtered_resources if resource['level'] == level_filter]
-        
-    return filtered_resources
+        filtered_resources = [resource for resource in filtered_resources
+                              if resource['level'] == level_filter]
+        return filtered_resources
+
 
 def get_daily_quote(category):
     '''Function to get a random quote from the ZenQuotes API'''
@@ -60,4 +69,3 @@ def get_daily_quote(category):
         return None
     except KeyError as e:
         return None
-        
